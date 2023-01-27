e0821e051e01b098219d01077a97666457db53cc
vinod kumar <vinodkumargolla001@gmail.com>
2023-01-27 18:57:21 +0530

    Changes done issue3

--- tech_home/models.py
+++ tech_home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    phone_number = models.CharField(max_length=20)
+    email_verified = models.BooleanField(default=False)
+    phone_verified = models.BooleanField(default=False)
+
