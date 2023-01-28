13c622b2d96e0e9eee0e0a4ddaf8b7513b7a72d6
VINODKUMARGOLLA <101210130+VINODKUMARGOLLA@users.noreply.github.com>
2023-01-28 10:23:11 +0530

    Changes done issue4 (#4)

--- tech_home/models.py
+++ tech_home/models.py
@@ -4,9 +4,34 @@ from django.db import models
 
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
+from tech_home.managers import CustomUserManager
+
 
 class CustomUser(AbstractUser):
+    email = models.EmailField(_('email address'), unique=True)
+    username = models.CharField(max_length=30, null=True)
+    first_name = models.CharField(max_length=30, null=True)
+    last_name = models.CharField(max_length=30, null=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+    updated_on = models.DateTimeField(default=timezone.now)
     phone_number = models.CharField(max_length=20)
     email_verified = models.BooleanField(default=False)
     phone_verified = models.BooleanField(default=False)
+
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        db_table = "TechUsers"
+
+
 
